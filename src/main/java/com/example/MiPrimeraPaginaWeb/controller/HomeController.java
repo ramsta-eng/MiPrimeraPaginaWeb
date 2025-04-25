8447eec866135af2eaefea51ea61c25dff363341
IvanS <iv.santander@duocuc.cl>
2025-04-25 19:54:07 -0400

    Combio de carpita controlador

--- src/main/java/com/example/MiPrimeraPaginaWeb/controller/HomeController.java
+++ src/main/java/com/example/MiPrimeraPaginaWeb/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.MiPrimeraPaginaWeb;
+package com.example.MiPrimeraPaginaWeb.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
