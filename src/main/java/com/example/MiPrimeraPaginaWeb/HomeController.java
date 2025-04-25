efd039b1160fa9426522dc59f2e99ceb188fb993
IvanS <iv.santander@duocuc.cl>
2025-04-24 21:51:07 -0400

    Se cambio directorio y se actualiza a version v2

--- src/main/java/com/example/MiPrimeraPaginaWeb/HomeController.java
+++ src/main/java/com/example/MiPrimeraPaginaWeb/HomeController.java
@@ -2,11 +2,12 @@ package com.example.MiPrimeraPaginaWeb;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-
+@RequestMapping("/api/v2")
 public class HomeController {
 
     @GetMapping("/")    
